a76f5420c9e128379295c4dd1550f1769faa4684
gongjie <1173047200@qq.com>
2023-06-20 20:59:55 +0800

    工具类fix重试逻辑

--- kgboot-util/src/main/java/com/gjie/kgboot/util/log/CommonUtils.java
+++ kgboot-util/src/main/java/com/gjie/kgboot/util/log/CommonUtils.java
@@ -1,12 +1,19 @@
 package com.gjie.kgboot.util.log;
 
+import com.alibaba.fastjson.JSON;
+import com.gjie.kgboot.common.constant.ErrorEnum;
+import com.gjie.kgboot.common.exception.BaseException;
 import com.gjie.kgboot.util.log.bo.BaseTask;
 
+import java.util.Arrays;
 import java.util.concurrent.DelayQueue;
 import java.util.function.Function;
 
-public class CommonUtils<M, N> {
-    public Runnable retry(Integer num, Long initDelay, Function<M, N> function, M m, N expectedResult) {
+public class CommonUtils {
+    public static <M, N> Runnable retry(Integer num, Long initDelay, Function<M, N> function, M m, N... expectedResults) {
+        if (expectedResults == null) {
+            throw new BaseException("重试期望值不可为空");
+        }
         DelayQueue<BaseTask<M, N>> baseTasks = new DelayQueue<>();
         if (num == 0) {
             num = 5;
@@ -25,19 +32,23 @@ public class CommonUtils<M, N> {
         }
         //执行
         return () -> {
+            Exception exception = null;
             while (!baseTasks.isEmpty()) {
                 try {
                     baseTasks.take();
                     N apply = function.apply(m);
-                    if (expectedResult.equals(apply)) {
+                    if (Arrays.asList(expectedResults).contains(apply)) {
                         return;
                     }
                 } catch (Exception e) {
-                    //todo 记录错误日志
-                    e.printStackTrace();
+                    exception = e;
                 }
             }
-            throw new RuntimeException("重试失败");
+            if (exception != null) {
+                throw new BaseException(ErrorEnum.METHOD_EXECUTE_ERROR, exception.getMessage());
+            }
+            throw new BaseException(ErrorEnum.METHOD_EXECUTE_ERROR, "执行不满足期望值:" + JSON.toJSONString(Arrays.asList(
+                    expectedResults)));
         };
     }
 }
