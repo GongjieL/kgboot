814cc367a573f609ee11985f4916393eeba80bb9
gongjie <1173047200@qq.com>
2023-06-21 00:39:38 +0800

    remove参数

--- kgboot-api/src/main/java/com/gjie/kgboot/api/config/redis/KgBootRedisConfig.java
+++ kgboot-api/src/main/java/com/gjie/kgboot/api/config/redis/KgBootRedisConfig.java
@@ -1,11 +1,18 @@
 package com.gjie.kgboot.api.config.redis;
 
 import com.gjie.kgboot.api.client.redis.KgBootRedisClient;
+import org.redisson.Redisson;
+import org.redisson.config.ClusterServersConfig;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(KgBootRedisProperties.class)
@@ -14,6 +21,9 @@ public class KgBootRedisConfig {
     @Autowired
     private KgBootRedisProperties kgBootRedisProperties;
 
+    @Value(("${spring.redis.cluster.nodes}"))
+    private String redisNodes;
+
     @Bean
     public KgBootRedisClient kgBootRedisClient() {
         KgBootRedisClient kgBootRedisClient = new KgBootRedisClient();
@@ -21,4 +31,22 @@ public class KgBootRedisConfig {
                 200l : kgBootRedisProperties.getDelayTime());
         return kgBootRedisClient;
     }
+
+    @Bean
+    @ConditionalOnProperty("spring.redis.cluster.nodes")
+    public Redisson redisson() {
+        //获取ip地址
+        String[] nodes = redisNodes.split(",");
+        //redisson版本是3.5，集群的ip前面要加上“redis://”，不然会报错，3.2版本可不加
+        List<String> clusterNodes = new ArrayList<>();
+        for (int i = 0; i < nodes.length; i++) {
+            clusterNodes.add("redis://" + nodes[i]);
+        }
+        Config config = new Config();
+        ClusterServersConfig clusterServersConfig = config.useClusterServers()
+                .addNodeAddress(clusterNodes.toArray(new String[clusterNodes.size()]));
+//        clusterServersConfig.setPassword(redisConfigProperties.getPassword());//设置密码，如果没有密码，则注释这一行，否则启动会报错
+        return (Redisson) Redisson.create(config);
+    }
+
 }
